7f27f4cf1016ffe9f03e7d09c742571351e02443
장석원 <sc204120@shinhan.com>
2025-05-09 09:46:59 +0900

    chore: ts config

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      // `src` 디렉토리를 기준으로 각 경로 별칭 정의
+      '@components': '/src/components',
+      '@styles': '/src/styles',
+    },
+  },
 })
